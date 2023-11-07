2533191bb9268454dab57670b0e340fb435d9378
namo.fang <namo.fang@ixiaoshuidi.com>
2023-11-07 17:46:02 +0800

    fix:字段修改

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,8 +1,8 @@
 import { json } from '@sveltejs/kit'
 
 export async function load({ fetch }) {
-  const user = await fetch('/user')
+  const result = await fetch('/user')
   return {
-    user: json(user)
+    result: json(result)
   }
 }
