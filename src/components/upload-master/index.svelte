75accb1d2c689893901753366bab7c1e3260c595
namo.fang <namo.fang@ixiaoshuidi.com>
2023-11-17 17:40:04 +0800

    七牛云文件删除

--- src/components/upload-master/index.svelte
+++ src/components/upload-master/index.svelte
@@ -1,12 +1,14 @@
 <script lang="ts">
   import QiniuUpload from './qiniu-upload/index.svelte'
+	import type { Label } from './qiniu-upload/utils';
 
   export let type = 'image'
   export let platform = 'qiniu'
+  export let region: Label = '华东-浙江';
 </script>
 
 <div class="upload-master">
   {#if platform == 'qiniu'}
-    <QiniuUpload {type} />
+    <QiniuUpload {type} {region} />
   {/if}
 </div>
--- src/components/upload-master/qiniu-upload/index.svelte
+++ src/components/upload-master/qiniu-upload/index.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
+  import dayjs from 'dayjs';
+	import { makeUploadDomain, makeManageDomain, type Label } from "./utils";
+
   export let type = '';
+  export let region: Label;
 
-  const body = new FormData()
-
-  const change = async (e: Event)=> {
+  const changeHandle = async (e: Event)=> {
     const $input = e.target as HTMLInputElement
 
     if(!$input.files || $input.files.length !== 1) {
@@ -14,13 +16,17 @@
 
     const name = file.name;
 
-    const result = await fetch('http://localhost:8080/upload/qiniu', {
-      method: 'POST',
+    const method = 'POST';
+
+    const body = new FormData()
+
+    const result = await fetch('http://localhost:8080/storage/qiniu/upload-token', {
+      method,
       body: JSON.stringify({
         name
       }),
       headers: {
-        'Content-Type': 'application/json; charset=utf-8'
+        'Content-Type': 'application/json'
       }
     })
 
@@ -31,10 +37,9 @@
       body.append('token', data.token)
       body.append('file', file)
 
-      const result = await fetch('https://upload-z0.qiniup.com', {
+      const result = await fetch(makeUploadDomain(region), {
         body,
-        method: 'POST',
-        mode: 'cors',
+        method,
       })
 
       const data2 = await result.json()
@@ -42,10 +47,84 @@
       console.log(data2)
     }
   }
+
+  const deleteHandle = async ()=> {
+    const method = 'POST'
+    const name = 'pdf.png'
+
+    const path = btoa(name);
+
+    const url = `${makeManageDomain(region)}/delete/${path}`
+
+    const { hostname, pathname, search } = new URL(url);
+
+    const headers: {[k: string]: string} = {
+      'X-Qiniu-Date': dayjs().format('YYYYMMDDTHHmmss') + 'Z',
+    }
+
+    const pieces = [
+      search
+        ? `${method.toUpperCase()} ${pathname}${search}`
+        : `${method} ${pathname}`,
+        `Host: ${hostname}`,
+        `Content-Type: application/x-www-form-urlencoded`,
+    ];
+
+    if (headers) {
+      const keys = Object.keys(headers).filter(
+        (key) => key.indexOf('X-Qiniu-') === 0,
+      );
+      keys.forEach((key) => {
+        pieces.push(`${key}: ${headers[key]}`);
+      });
+    }
+
+    pieces.push('');
+    pieces.push('');
+
+    // if (body) {
+    //   pieces.push(body);
+    // }
+
+    const body = pieces.join('\n')
+
+    console.log('body', body)
+
+    const result = await fetch('http://localhost:8080/storage/qiniu/delete-token', {
+      body: JSON.stringify({
+        body
+      }),
+      method,
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    })
+
+    const { success, data } = await result.json();
+
+    if(success) {
+      const qiniuResult = await fetch(url, {
+        method,
+        headers: {
+          ...headers,
+          'Content-Type': 'application/x-www-form-urlencoded',
+          Authorization: `Qiniu ${data.token}`,
+        }
+      })
+
+      const qiniuData = await qiniuResult.json()
+
+      console.log('qiniuData', qiniuData);
+    }
+  }
 </script>
 
 <div class="">
-  <input type="file" on:change={ change }/>
+  <input type="file" on:change={ changeHandle }/>
 
   <input type="hidden" data-type={ type }/>
+</div>
+
+<div>
+  <button type="button" on:click={ deleteHandle }>删除</button>
 </div>
