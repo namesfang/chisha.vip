5e6c05a0cff8f4f6162c0c2736be752f9bb844d1
namo.fang <namo.fang@ixiaoshuidi.com>
2023-11-20 17:49:14 +0800

    feat:CDN资源公共引入

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,12 +1,33 @@
 import type { Handle } from "@sveltejs/kit";
 
+type Prepls = {
+  [key: string]: (prod: boolean, origin: string)=> string
+}
+
+const prepls: Prepls = {
+  // CDN资源
+  '%cdn.assets%': (prod, origin)=> {
+    if(prod) {
+      return 'http://static.chisha.vip';
+    }
+    return origin;
+  },
+  // 版权
+  '%copyright%': ()=> {
+    return '@'+ String(new Date().getFullYear());
+  }
+}
+
 export const handle: Handle = async ({ event, resolve })=> {
   // event.locals.user = {
   //   a: 1312
   // }
   return resolve(event, {
     transformPageChunk: ({ html })=> {
-      return html.replaceAll('%copyright%', '@'+String(new Date().getFullYear()))
+      Object.keys(prepls).forEach((n, i)=> {
+        html = html.replaceAll(n, Object.values(prepls)[i](process.env.NODE_ENV === 'production', event.url.origin))
+      })
+      return html;
     },
   })
 }
